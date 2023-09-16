c9a256f45fdc855d5fbdfd00c4489538e83adf3d
Ahammed Irfan Ali <69192055+irfanpa005@users.noreply.github.com>
2023-09-16 22:13:04 +0530

    project 3

--- project3/index.js
+++ project3/index.js
@@ -1,3 +1,4 @@
+
 // const avatarElement = document.getElementById("avatar");
 // const usernameElement = document.getElementById("username");
 // const repoListElement = document.getElementById("repo-list");
@@ -21,11 +22,22 @@
 //     });
 //   });
 
+
+
+
 fetch(`https://api.github.com/users/irfanpa005/repos`)
 .then(response => response.json())
 .then(data =>{
-    console.log(data)
+    const listDiv = document.getElementById("repo-list");
     data.forEach(repo => {
-        console.log(repo.html_url)
+        var repoDiv = document.createElement('div');
+        repoDiv.className ="card my-2";
+        repoDiv.innerHTML = `
+        <p>Name: ${repo.name}</p>
+        <p>Link: ${repo.html_url}</p>
+        <a class="btn btn-info text-white" href="${repo.html_url}"}>View</a>
+        `;
+        listDiv.appendChild(repoDiv)
+        console.log(repoDiv)
     })
 });
